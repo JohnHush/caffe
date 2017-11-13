981d9cad5cafd881b1a18f37e4fb718e593f07d4
John Hush <luohenggogogo@aliyun.com>
2017-11-13 16:32:43 +0800

    delete some *gpu function in multi label loss files

--- include/caffe/layers/multi_label_sigmoid_cross_entroy_loss.hpp
+++ include/caffe/layers/multi_label_sigmoid_cross_entroy_loss.hpp
@@ -43,16 +43,12 @@ class MultiLabelSigmoidCrossEntropyLossLayer : public LossLayer<Dtype> {
   /// @copydoc SigmoidCrossEntropyLossLayer
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
 
   /**
 	 * #TODO
 	 * should be finished if you wanna generate GEXGEN file
    */
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
 
   /// Read the normalization mode parameter and compute the normalizer based
