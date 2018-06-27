bead4949fcb2a6690534d0f42b7391e97d4a6d2c
John Hush <luohenggogogo@aliyun.com>
2018-06-27 15:37:22 +0800

    try to merge multple labels classification feature totally into faster rcnn branch cause this feature is implemented based on SSD befor and is trained on SSD branch

--- examples/MULTI_LABEL/train_PA100K_GPU_resnet18.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-/home/jh/working_lib/caffe/build/tools/caffe train --solver=./solver_GPU_resnet18.prototxt --weights=/home/jh/working_data/models/ResNet-18-Caffemodel-on-ImageNet/resnet-18.caffemodel -gpu=0,1
---
+++ examples/multi_label_PA100K/train_PA100K_GPU_resnet18.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+set -e
+
+/home/jh/working_lib/caffe/build/tools/caffe train --solver=./solver_GPU_resnet18.prototxt --weights=/home/jh/working_data/ResNet-18-Caffemodel-on-ImageNet/resnet-18.caffemodel -gpu=2
